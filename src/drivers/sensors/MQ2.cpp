6974eaa6307c5286b8e5c1cfb7c79c70321b7ae7
shawn_phy <61213955+shawn-phy@users.noreply.github.com>
2023-11-03 04:35:04 +0000

    update directory structure.

---
+++ src/drivers/sensors/MQ2.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h> 
+const int GAS_LED = 4;
+const int MQ2_INPUT = 2; 
+
+void setup() {
+  Serial.begin(115200);  
+  pinMode(GAS_LED, OUTPUT);
+}
+
+bool Gas_detected(){
+  int sensorout = analogRead(MQ2_INPUT); 
+    if (sensorout > 1800) {   
+    Serial.println("Gas");  
+    digitalWrite (GAS_LED, HIGH) ; 
+      return true; 
+  }
+  else {
+    Serial.println("No Gas");
+    digitalWrite (LED, LOW) ; 
+    return false; 
+  }
+}
+  
