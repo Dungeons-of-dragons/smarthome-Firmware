5fa72d2210c771a20ff67bf37ac1620fdc860c4d
Barbra Gitonga <gitongabarbra@gmail.com>
2023-11-09 16:48:47 +0300

    Door edited and decoupled

--- lib/RFID/Door.cpp
+++ lib/RFID/Door.cpp
@@ -6,12 +6,11 @@
 #include <MFRC522.h>
 #include "defs.h"
 
-
-// initializing the RFID card reader
+// setup
 MFRC522 mfrc522(SS_PIN, RST_PIN);  
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup() 
+void initialize() 
 {
   pinMode(buzzer, OUTPUT);
   pinMode(lock, OUTPUT);
@@ -22,37 +21,15 @@ void setup()
   lcd.begin(16,2);
   lcd.clear(); 
   lcd.print("Scan Card!");
+
   // initialize RFID card reader
   mfrc522.PCD_Init();   
   Serial.println();
 }
 
 
-bool checkcardispresent(){ 
-   
-}
-void loop() 
-{
-  // Check if card is available for reading
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
-  }
-  // Read the serial number of the card/tag
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
-  }  
-  //Ask user to scan card
-  lcd.begin(16,2);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Scan card");
-  Serial.print("UID tag :");
-
-  // printing out the cards information to serial
+bool check_card_is_present(){ 
   String content= "";
-  byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -64,8 +41,6 @@ void loop()
   Serial.print("Message : ");
   content.toUpperCase();
   lcd.print("Scan Card");    
-
-  // Checks if the serial number in the card is given authorization (take information from DB)
   if (content.substring(1) == "73 78 D3 1C") // Make sure you change this with your own UID number
   {
     //access granted
@@ -76,23 +51,47 @@ void loop()
     lcd.println("Authorised access");    
     digitalWrite(lock, LOW);
     Serial.println();
-    delay(1000);
-    digitalWrite(buzzer, LOW);
+    delay(1000);  
+    digitalWrite(buzzer, HIGH);
     delay(5000);
     digitalWrite(lock, HIGH); // if relay is closed HIGH
-    delay(2500);    
+    delay(2500);
+    return true;
   }
- 
-  // Access Denied
   else {
     Serial.println(" Access denied");
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.println("Access denied");
     digitalWrite(lock, LOW); // if relay is open LOW
-    delay(1000);    
+    delay(1000);  
+    return false;  
   }
-lcd.clear();
-lcd.setCursor(0,0);
-lcd.println("Scan Card");
 }
+
+void check_for_card() 
+{
+  // Check if card is available for reading
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Read the serial number of the card/tag
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }  
+}
+  
+void lcd_scan() {
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Scan card");
+  Serial.print("UID tag :");
+  }
+  // Ask user to scan card
+
+// lcd.clear();
+// lcd.setCursor(0,0);
+// lcd. ("Scan Card");
--- lib/RFID/door.h
+++ lib/RFID/door.h
@@ -1,4 +1,8 @@
 #ifndef DOOR_H
 #define DOOR_H
 
+void initialize();
+void check_for_card();
+bool check_card_is_present();
+
 #endif
