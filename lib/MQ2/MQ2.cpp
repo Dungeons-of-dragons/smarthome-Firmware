414fe711597223a1b51bcb3b479432cf7982bcd5
Barbra Gitonga <gitongabarbra@gmail.com>
2023-11-09 13:58:13 +0300

    MQ2.h added

--- lib/MQ2/MQ2.cpp
+++ lib/MQ2/MQ2.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h> 
 #include "defs.h"
 
+volatile boolean gaspresent = false;
+
 bool Gas_detected(){
   pinMode(GAS_LED, OUTPUT);
   int sensorout = analogRead(MQ2_INPUT); 
     if (sensorout > 1800) {   
     Serial.println("Gas");  
-    digitalWrite (GAS_LED, HIGH) ; 
+    digitalWrite (GAS_LED, HIGH); 
+    gaspresent = true;
+
       return true; 
   }
   else {
