e6a028271db5e80f6484f87e9c9ec189feb98205
Barbra Gitonga <gitongabarbra@gmail.com>
2023-11-10 13:17:13 +0300

    reorganised directory

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Firmware
-a repo for all firmware development for the esp32
+A repo for all firmware development for the esp32
+
+Note: Use ESP32 core 2.0.7 due to the MFRC522 library
--- include/door.h
+++ include/door.h
@@ -1,10 +1,14 @@
 #ifndef DOOR_H
 #define DOOR_H
 
+#include "Arduino.h"
+
 #include "defs.h"
 
 void initialize();
 void check_for_card();
 bool card_authorization();
+void display_tag();
 
 #endif
+
