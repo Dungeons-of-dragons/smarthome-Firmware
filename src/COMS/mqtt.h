4f56ab7972eb7345e09a9658a62aa852cf854219
Barbra Gitonga <gitongabarbra@gmail.com>
2023-11-16 12:58:12 +0300

    removed Topicd directory

--- src/COMS/mqtt.h
+++ src/COMS/mqtt.h
@@ -2,6 +2,13 @@
 #define MQTT_H
 
 
+//Topics to be published
+const char* Temperature = "/sensors/temperature/";
+const char* Humidity = "/sensors/humidity/";
+const char* Watts = "/sensors/watts/";
+const char* Current = "/sensors/current/";
+const char* Gas = "/sensors/gas/";
+
 void mqtt_setup(); 
 void callback();
 
--- src/TOPICS/Topics.cpp
+++
@@ -1,3 +0,0 @@
-#include "main.h"
-#include "defs.h"
-
