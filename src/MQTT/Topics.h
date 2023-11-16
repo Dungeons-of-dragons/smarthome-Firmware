3abe55643c1326dcb248265f9d9b5eae6f9dc61e
Barbra Gitonga <gitongabarbra@gmail.com>
2023-11-16 03:38:48 +0300

    added topics

---
+++ src/MQTT/Topics.h
@@ -0,0 +1,10 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+const char* Temperature = "/sensors/temperature/";
+const char* Humidity = "/sensors/humidity/";
+const char* Watts = "/sensors/watts/";
+const char* Current = "/sensors/current/";
+const char* Gas = "/sensors/gas/";
+
+#endif
