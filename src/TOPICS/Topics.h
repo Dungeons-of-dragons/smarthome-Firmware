f49c8007d81956663e3d2f346567c7be1a3ad6fa
Barbra Gitonga <gitongabarbra@gmail.com>
2023-11-16 12:32:35 +0300

    topicd created

--- src/TOPICS/Topics.h
+++ src/TOPICS/Topics.h
@@ -1,10 +1,12 @@
-#ifndef MQTT_H
-#define MQTT_H
+#ifndef TOPICS_H
+#define TOPICS_H
 
+//Topics to be published
 const char* Temperature = "/sensors/temperature/";
 const char* Humidity = "/sensors/humidity/";
 const char* Watts = "/sensors/watts/";
 const char* Current = "/sensors/current/";
 const char* Gas = "/sensors/gas/";
+
 
 #endif
